22c0a05d2dc9a79069542b0b122b2b65ba44e437
aliya-sketch <aliyaaziz609@gmail.com>
2026-09-09 23:56:44 +0500

    Create Task 5 .py

---
+++ Task 5 .py
@@ -0,0 +1,14 @@
+# Q5 Explain the given code
+
+age = 20
+item = 5
+print("age :", age)
+print("Items :", item)
+
+# Here we convert int to float using float()
+
+age = float(age)    
+item = float(item)
+
+print("age :", age)
+print("Items :", item)
