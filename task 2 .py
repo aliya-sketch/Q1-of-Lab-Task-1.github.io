9e7464b8a0a14e1a6d8d9504b1a9ec06bacbeb97
aliya-sketch <aliyaaziz609@gmail.com>
2026-09-09 23:51:08 +0500

    Create task 2 .py

---
+++ task 2 .py
@@ -0,0 +1,7 @@
+# Program to calculate gross pay..
+
+hours = float(input("Enter hours worked: "))
+rate = float(input("Enter hourly rate: "))
+
+gross_pay = hours * rate
+print("Your gross pay is:", gross_pay)
