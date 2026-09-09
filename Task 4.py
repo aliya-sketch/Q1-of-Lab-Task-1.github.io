404b54856161547e754e0e53774011e72fe04301
aliya-sketch <aliyaaziz609@gmail.com>
2026-09-09 23:55:14 +0500

    Create Task 4.py

---
+++ Task 4.py
@@ -0,0 +1,7 @@
+# Q4 Program for exponentiation - power
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+add = num1 ** num2
+print(num1, "raised to the power of", num2, "is:", add)
